09a5267ffe244ccb777d66fc6c14a568289d60e3
ivacker@outlook.com <ivacker@outlook.com>
2023-02-22 17:22:57 -0300

    sincronizacion

--- go.mod
+++ go.mod
@@ -2,4 +2,6 @@ module ivacker.dev/go-mp3
 
 go 1.19
 
-require github.com/gorilla/mux v1.8.0
+require github.com/gorilla/handlers v1.5.1
+
+require github.com/felixge/httpsnoop v1.0.1 // indirect
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"html/template"
 	"math/rand"
 	"net/http"
@@ -8,45 +9,68 @@ import (
 	"path/filepath"
 	"time"
 
-	"github.com/gorilla/mux"
+	"github.com/gorilla/handlers"
 )
 
 func main() {
 
-	// header := handlers.AllowedHeaders([]string{"X-Requested-With", "Content-Type", "Authorization"})
-	// methods := handlers.AllowedMethods([]string{"GET", "POST", "PUT", "HEAD", "OPTIONS"})
-	// origins := handlers.AllowedOrigins([]string{"*"})
+	header := handlers.AllowedHeaders([]string{"X-Requested-With", "Content-Type", "Authorization"})
+	methods := handlers.AllowedMethods([]string{"GET", "POST", "PUT", "HEAD", "OPTIONS"})
+	origins := handlers.AllowedOrigins([]string{"*"})
 
-	r := mux.NewRouter()
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 
-	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		dir := "P:/evolmusic/mp3"
-		dirParam := "mp3"
-		vars := mux.Vars(r)
+		if r.URL.Path == "/" {
+			canciones, err := listarCanciones()
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
 
-		if dirParam, ok := vars["dir"]; ok {
-			dir = "P:/evolmusic/" + dirParam
-		}
+			// Si se proporcionó un parámetro en la URL, reproducir esa canción primero
+			if cancionParam := r.URL.Query().Get("cancion"); cancionParam != "" {
+				cancionPath := "P:/evolmusic/mp3/" + cancionParam
 
-		canciones, err := listarCanciones(dir, dirParam)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
+				// Servir el archivo de la canción solicitada
+				http.ServeFile(w, r, cancionPath)
+
+				// Esperar a que la canción termine de reproducirse antes de continuar
+				// la lista de reproducción
+				time.Sleep(time.Second)
+			}
+
+			renderTemplate(w, canciones)
 			return
 		}
 
-		renderTemplate(w, canciones)
-		http.ServeFile(w, r, dir)
+		filePath := r.URL.Path[1:]
 
-		
-	}).Methods("GET")
+		if _, err := os.Stat(filePath); os.IsNotExist(err) {
+			http.NotFound(w, r)
+			return
+		}
+		http.ServeFile(w, r, filePath)
+	})
 
-	else {
-		
+	// Ruta base para la carpeta public
+	publicFS := http.FileServer(http.Dir("p:/evolmusic/mp3/"))
+	http.Handle("/mp3/", http.StripPrefix("/mp3/", publicFS))
+
+	// Ruta base para la carpeta assets
+	assetsFS := http.FileServer(http.Dir("p:/evolmusic/js/"))
+	http.Handle("/js/", http.StripPrefix("/js/", assetsFS))
+
+	//SSL
+	//C:\Certbot\live\mp3.ivacker.xyz\fullchain.pem
+
+	//go http.ListenAndServe(":80", nil)
+
+	err1 := http.ListenAndServeTLS(":443", "C:/Certbot/live/mp3.ivacker.dev/fullchain.pem", "C:/Certbot/live/mp3.ivacker.dev/privkey.pem", handlers.CORS(header, methods, origins)(http.DefaultServeMux)) // SSL (Utilizo certificado para SSL)
+	fmt.Println("Servidor 443...")
+	if err1 != nil {
+		fmt.Print(err1)
 	}
 
-	http.Handle("/", r)
-
-	http.ListenAndServe(":80", nil)
 }
 
 type cancion struct {
@@ -54,16 +78,16 @@ type cancion struct {
 	URL    string
 }
 
-func listarCanciones(sRuta string, sDir string) ([]cancion, error) {
+func listarCanciones() ([]cancion, error) {
 	rand.Seed(time.Now().UnixNano())
 
 	var canciones []cancion
-	err := filepath.Walk(sRuta, func(path string, info os.FileInfo, err error) error {
+	err := filepath.Walk("P:/evolmusic/mp3/", func(path string, info os.FileInfo, err error) error {
 		if err != nil {
 			return err
 		}
 		if !info.IsDir() {
-			canciones = append(canciones, cancion{Nombre: info.Name(), URL: "/" + sDir + "/" + info.Name()})
+			canciones = append(canciones, cancion{Nombre: info.Name(), URL: "/mp3/" + info.Name()})
 		}
 		return nil
 	})
@@ -83,4 +107,16 @@ func renderTemplate(w http.ResponseWriter, canciones []cancion) {
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
+}
+
+func redirectToHTTPSHandler(domain string) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		http.Redirect(w, r, "https://"+domain+r.RequestURI, http.StatusMovedPermanently)
+	}
+}
+
+func mainHandler(w http.ResponseWriter, r *http.Request) {
+
+	redirectToHTTPSHandler("https://mp3.ivacker.xyz")(w, r)
+
 }
